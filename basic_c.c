5b50d8c701a97a82e176cf0368fc4ed275929448
KarishmaJ30 <karishmajaunjalkar8@gmail.com>
2026-09-12 22:41:09 +0530

    Implement factorial calculation in basic_c.c
    Added a factorial program to calculate the factorial of a user-input number.

--- basic_c.c
+++ basic_c.c
@@ -13,3 +13,18 @@ int main(){
 
   return 0 ;
 } 
+
+// factorial programme
+
+#include<stdio.h>
+int main(){
+  int a ;
+  printf("Enter a number: ") ;
+  scanf("%d",&a) ;
+
+  int fact = 1 ;
+
+  for(int i=1;i<=a;i++)
+    fact = fact*i ;
+  printf("factorial = %d",fact) ;
+}
