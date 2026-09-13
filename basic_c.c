55f5755b92fa2bd3d115a298022ab3ab8d5f0cff
KarishmaJ30 <karishmajaunjalkar8@gmail.com>
2026-09-13 21:43:11 +0530

    Add factorial and swapping functions in basic_c.c

--- basic_c.c
+++ basic_c.c
@@ -27,4 +27,24 @@ int main(){
   for(int i=1;i<=a;i++)
     fact = fact*i ;
   printf("factorial = %d",fact) ;
+
+  return 0 ;
+}
+
+
+// swapping
+
+#include<stdio.h>
+int main(){
+  int a , b ;
+  printf("Enter two number: ") ;
+  scanf("%d%d",&a,&b) ;
+
+  int temp = a ;
+  a = b ;
+  b = temp ;
+
+  printf("After swapping: %d , %d",a,b) ;
+
+  return 0 ;
 }
